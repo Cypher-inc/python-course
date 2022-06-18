a4f230e9c033b0256543ad6300fc2fa410236a21
Cypher-inc <victorynorden@gmail.com>
2022-06-18 18:32:33 +0530

    day2

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import math
+from tkinter.tix import Tree
 
 print('Hello World!')
 
@@ -158,10 +159,96 @@ housePrice = 1000000
 userSal = 1
 userCredit = 0
 
-if userSal and userCredit:
+if userSal or userCredit:
     print('Eligible for loan')
 else:
     print('NOT eligible for loan')
 
+userName = 'Kris'
+if len(userName) < 2:
+    print('Length should be altleast 3! ')
+elif len(userName) >= 6:
+    print('Length cannot be over 50!')
+else:
+    print('Name looks good!')
 
 
+# userWeight = float(input('Enter user weight? '))
+# userUnit = input("Do you prefer Kg or Lb? ")
+
+# if userUnit.lower() == 'kg':
+#     print(round(userWeight * 0.453592))
+# elif userUnit.lower() == 'lb':
+#     print(round(userWeight * 2.20462))
+# print(userWeight)
+
+count = 0
+
+# while True:
+#     i = int(input('Do you want to continue? '))
+#     if i:
+#         count += 1
+#         continue
+#     else:break
+# print(count)
+
+# while True:
+#     i = int(input('Enter a number? '))
+#     if i==5:
+#         print('You guessed the number!')
+#         break
+#     else:continue
+
+# shapes using while
+
+# i=5
+# while i>=1:
+#     print('*'*i)
+#     i=i-1
+
+# i=0
+# while i<=5:
+#     print('*'*i)
+#     i=i+1
+
+# Car game
+# started = False
+# stopped = False
+# while True:
+#     startInput = input('>')
+#     if startInput.lower() == "start":
+#         if started == True:
+#             print('Warning: Car has ALREADY started!')
+#         else:
+#             print('Car has started!')
+#             started = True
+#     elif startInput.lower() == "stop":
+#         if stopped == True:
+#             print("Warning: Car has ALREADY stopped")
+#         else:
+#             print("Car has stopped")
+#             stopped = True
+#     elif startInput.lower() == "quit":
+#         break
+#     else:
+#         print("I don't understand that..")
+
+#for loop
+
+# for x in 'python':
+#     print(x)
+
+# arr = ['Kris','John','Mike','James']    
+# for names in arr:
+#     print(names)
+
+# #range
+# for items in range(10):
+#     print(items)    
+
+# for items in range(2,11):
+#     print(items)    
+
+#steps
+for items in range(0,10,2):
+    print(items) 
