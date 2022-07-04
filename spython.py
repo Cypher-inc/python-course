9e9cbf44dade051287eaf1529dd9f2312fc9eb2e
Cypher-inc <victorynorden@gmail.com>
2022-07-04 19:55:42 +0530

    day17

--- spython.py
+++ spython.py
@@ -1015,18 +1015,32 @@ print('-------Day 9-------')
 
 
 #division sun pairs
-lst1 = list(map(int,input().split(' ')))
-lst2 = list(map(int,input().split(' ')))
+# lst1 = list(map(int,input().split(' ')))
+# lst2 = list(map(int,input().split(' ')))
 
-# print(lst1[1])
-print(len(lst2))
+# # print(lst1[1])
+# print(len(lst2))
 
-count = 0
-for x in lst2:
-    for y in lst2:
-        # print(x,y)
-        if x < y:
-            if (x+y)%lst1[1] == 0:
-                # print(x,y)
-                count += 1
-print(count)            
+# count = 0
+# for x in lst2:
+#     for y in lst2:
+#         # print(x,y)
+#         if x < y:
+#             if (x+y)%lst1[1] == 0:
+#                 # print(x,y)
+#                 count += 1
+# print(count)
+ 
+###soln           
+# lst1 = list(map(int,input().split(' ')))
+# lst2 = list(map(int,input().split(' ')))
+# def divisibleSumPairs(n, k, ar):
+#     # Complete this function
+#     count = 0
+#     for i in range(n-1):
+#         j = i+1
+#         while j < n:
+#             if ((ar[i] + ar[j]) % k) == 0:
+#                 count += 1
+#             j += 1
+#     return count
