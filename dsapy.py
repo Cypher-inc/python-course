9e92a492f656539bc377f375383e41c4b19a5919
Cypher-inc <victorynorden@gmail.com>
2022-07-20 16:32:13 +0530

    Day23

---
+++ dsapy.py
@@ -0,0 +1,19 @@
+from jmespath import search
+
+
+
+#Array
+lst1 = [ 2, 4, 5, 7]
+
+# To find an element 
+print(lst1[2]) #O(1)
+
+# for x in lst1: #O(n)
+
+#for x in lst1:
+#for y in x   #O(n^2)
+
+# Using binary search O(log n)
+
+#insert
+# lst1.insert(1,256)++
